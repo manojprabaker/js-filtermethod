9186a1df5e3a0ba24f5435254e13a2b9ce93c4b4
manojprabaker <manojprabaker999@gmail.com>
2023-03-19 20:56:44 +0530

    add

---
+++ main.js
@@ -0,0 +1,21 @@
+const employees = [
+  { name: 'David Carlson', age: 30 },
+  { name: 'John Cena', age: 34 },
+  { name: 'Mike Sheridan', age: 25 },
+  { name: 'John Carte', age: 50 }
+];
+
+
+
+
+
+// const eD = employees.filter(function(detail) {
+   // return detail.age > 30;
+// });
+
+// console.log(eD);
+
+
+const eA=employees.filter(aGe=>aGe.age>30);
+
+console.log(eA);
